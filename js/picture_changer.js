1b32a77be242d8aea6c92c5547456d7e4c42008e
estherwolf <ewolf@umass.edu>
2016-12-11 20:03:01 -0500

    caching enabled, longer display time

--- js/picture_changer.js
+++ js/picture_changer.js
@@ -13,7 +13,7 @@ $.ajax({
           var _data = data;
           var d = new Date();
           set_pic(_data.resources[0].public_id);
-          duration = ((_data.resources.length)*3000)+1000;
+          duration = ((_data.resources.length)*30000)+1000;
         }
       });
 
@@ -35,5 +35,5 @@ $(document).ready(function(){
 
 function set_pic(name){
   var d = new Date();
-  var _url = "http://res.cloudinary.com/dse2nhyx3/image/upload/"+name;
+  var _url = "https://res.cloudinary.com/dse2nhyx3/image/upload/"+name;
     document.getElementById("monthImage").src = _url;}
