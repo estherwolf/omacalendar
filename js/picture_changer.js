9c3fafb4b2fc082f5ee6f6b8d6f37e0645ac261b
estherwolf <ewolf@umass.edu>
2016-12-19 01:18:49 -0500

    fixed next in calendar button, removed repeated calls on failure

--- js/picture_changer.js
+++ js/picture_changer.js
@@ -6,17 +6,21 @@ month_id = d.getMonth();
 
 var _url = 'https://res.cloudinary.com/dse2nhyx3/image/list/'+month_id+'month.json';
 var ind = 0;
+var success = false;
 var init_data;
+var interval_id;
+
 
 initialize(_url);
 handle_change_pic(_url);
+
 
 $('body').on('click', 'button.fc-next-button', function() {
   month_id = (month_id+1) % 12;
   _url = 'https://res.cloudinary.com/dse2nhyx3/image/list/'+month_id+'month.json';
   initialize(_url);
   handle_change_pic(_url);
-})
+});
 
 $('body').on('click', 'button.fc-prev-button', function() {
   if (month_id == 0) month_id = 12;
@@ -26,7 +30,8 @@ $('body').on('click', 'button.fc-prev-button', function() {
   _url = 'https://res.cloudinary.com/dse2nhyx3/image/list/'+month_id+'month.json';
   initialize(_url);
   handle_change_pic(_url);
-})
+
+});
 
 function initialize(_url){
 $.ajax({
@@ -36,14 +41,17 @@ $.ajax({
           init_data = data;
           var d = new Date();
           set_pic(init_data.resources[0].public_id);
-          duration = ((init_data.resources.length)*50000)+1000;
-        }
+          duration = ((init_data.resources.length)*10000)+1000;
+        },
+        error: function (request, status, error) {
+          set_default_pic(month_id);
+      }
       });
 }
 
 function handle_change_pic(_url){
-var interval_id = setInterval(function(){
-      $.ajax({
+ interval_id = setInterval(function(){
+      xhr = $.ajax({
         url: _url,
         success: function(data){
           var _data = data;
@@ -51,12 +59,24 @@ var interval_id = setInterval(function(){
           ind = d.getSeconds() % _data.resources.length;
           if((d.getHours() < 12 && d.getHours() >= 5) || (d.getHours() >= 4 && d.getHours()< 23)){
           set_pic(_data.resources[ind].public_id);}
+          set_pic(_data.resources[ind].public_id);
+
+        },
+        error: function (request, status, error) {
+          set_default_pic(month_id);
         }
         })
     }, duration);
 
 document.getElementById("prev_image").onclick = function() {prev_picture(ind)};
 document.getElementById("next_image").onclick = function() {next_picture(ind)};
+}
+
+function set_default_pic(name){
+    clearInterval(interval_id);
+    document.getElementById("monthImage").src = 'images/'+name+'.jpg';
+    xhr.abort();
+
 }
 
 function set_pic(name){
@@ -79,7 +99,3 @@ function next_picture(indx){
       ind = indx+1;
 
 }
-
-$('.fc-prev-button').click(function(){
-   alert('prev is clicked, do something');
-});
